d86bd393b4bd4d114b441037d7571a224eff1abb
Mayank Digarse <110493499+Mayank-Digarse@users.noreply.github.com>
2024-07-12 02:12:58 +0530

    Commit After database setup

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client"
+
 import { navLinks } from '@/constants'
-import {  SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -10,16 +11,15 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <aside className='sidebar'>
-        <div className='flex size-full flex-col gap-4'>
-            <Link href= "/" className='sidebar-logo' >
-            <Image src = "/assets/images/logo-text.svg" alt= "logo" width={180} height = {28} /
-            >
-            </Link>
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+        </Link>
 
-            <nav className='sidebar-nav'>
-              <SignedIn>
-              <ul className="sidebar-nav_elements">
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
               {navLinks.slice(0, 6).map((link) => {
                 const isActive = link.route === pathname
 
@@ -41,8 +41,9 @@ const Sidebar = () => {
                 )
               })}
               </ul>
-              
-              <ul className='sidebar-nav_elements'>
+
+
+            <ul className="sidebar-nav_elements">
               {navLinks.slice(6).map((link) => {
                 const isActive = link.route === pathname
 
@@ -63,22 +64,20 @@ const Sidebar = () => {
                   </li>
                 )
               })}
-              <li className='flex-center cursor-pointer gap-2 p-4'>
+
+              <li className="flex-center cursor-pointer gap-2 p-4">
                 <UserButton afterSignOutUrl='/' showName />
               </li>
+            </ul>
+          </SignedIn>
 
-              </ul>
-
-              </SignedIn>
-              
-              <SignedOut>
-                <Button asChild className='button bg-purple-gradient bg-cover'>
-                  <Link href="/sign-in">Login</Link>
-                </Button>
-              </SignedOut>
-
-            </nav>
-        </div>
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </nav>
+      </div>
     </aside>
   )
 }
